9b01b29c496b57146125575daae1dd2f555f64d5
Ylvakiller <rem.geuze@gmail.com>
2014-10-12 14:45:40 +0200

    Assignment1 of Lab1 done :smiling_imp:
    Whole assignment1 of lab 1 done

--- C/Assignment1/main.c
+++ C/Assignment1/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-
-int main(int argc, char **argv)
+void print_function( char letter );
+int ctr;
+int main( void )
 {
-	printf("hello world\n");
+	char star = '*';
+	char dash = '-';
+	for (ctr = 0 ; ctr < 10; ctr++ ) {
+		print_function( star );
+		print_function( dash );
+		printf("\n");
+	}
 	return 0;
 }
+void print_function ( char letter )
+{
+	int ctr;
+	for ( ctr = 0; ctr < 9; ctr++) {
+		printf("%c", letter);
+	}
+}
+//The programming error was that both the for loop in the main function aswell as the for loop in the print_function where using the same global variable, because of this only 1 line would be printed.
+//In order to solve this I gave the print_function a local variable, it now does print 10 lines.
+//The program in the print_function will print 9 characters of the type given to it
