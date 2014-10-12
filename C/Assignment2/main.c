aefdb948d3fa40141cdebda8295299c164b0c39c
Ylvakiller <rem.geuze@gmail.com>
2014-10-12 15:40:11 +0200

    got to remember to save not just build...
    still some user bugs on my end to sort out with codelight :)

--- C/Assignment2/main.c
+++ C/Assignment2/main.c
@@ -6,7 +6,12 @@ int main( void )
 
 	printf("How many numbers do you want to have in the first array?\n");
 	scanf("%d", &amount);
-
+	printf("Please enter your numbers, seperated by an enter:");
+	int i = 0;
+	while(i<amount){
+		
+		i++;
+	}
 	printf("%d\n", a);
 
 	return 0;
