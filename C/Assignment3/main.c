13a32de53464d557fa4ab55b470443c7932d6ca2
Ylvakiller <rem.geuze@gmail.com>
2014-10-17 22:21:23 +0200

    User communication for program3 nearly done

--- C/Assignment3/main.c
+++ C/Assignment3/main.c
@@ -2,6 +2,15 @@
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	printf("Hello there, I will give you a couple of options:\n");
+	_Bool running = 1;
+	while(running){
+		printf("You can terminate this program at any time by entering a negative number.\n");
+		printf("Otherwise you have the following options:\n");
+		printf("Option number 2: Sort some arrays\n");
+		printf("All other non-negative inputs will just be confirmed.\n");
+		printf("Meaning that these are just dummy procedures...\n");
+		printf("Please enter your number now:\n");
+	}
 	return 0;
 }
