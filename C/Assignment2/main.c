145cf9d6e6f9714fcf26547e27198bb1ae55e1b5
Ylvakiller <rem.geuze@gmail.com>
2014-10-17 21:52:59 +0200

    completed the creating of both arrays
    am now getting arrays and storing them in the program

--- C/Assignment2/main.c
+++ C/Assignment2/main.c
@@ -2,17 +2,32 @@
 
 int main( void )
 {
-	int amount;
+	int amount1;
 
 	printf("How many numbers do you want to have in the first array?\n");
-	scanf("%d", &amount);
-	printf("Please enter your numbers, seperated by an enter:");
+	scanf("%d", &amount1);
+	printf("Please enter your numbers, seperated by an enter:\n");
 	int i = 0;
-	while(i<amount){
-		
+	float one[amount1];
+	while(i<amount1){
+		scanf("%f", &one[i]);
 		i++;
 	}
-	printf("%d\n", a);
+	
+	int amount2;
+	printf("And how many in the second array?\n");
+	scanf("%d", &amount2);
+	float two[amount2];
+	printf("Enter your second set of numbers please:");
+	i=0;
+	while (i<amount2){
+		scanf("%f", &two[i]);
+		i++;
+	}
+	
+	
+	
+	//printf("%d\n", amount);
 
 	return 0;
 }
